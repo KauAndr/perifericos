d58acbd8997a167d420a437afe35d124dfa66468
André Unger <decounger0510@gmail.com>
2023-08-22 09:27:51 -0300

    Ambiente Desenvolvimento

---
+++ src/main/java/com/tcc/perifericos/perifericos/PerifericosApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.perifericos.perifericos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerifericosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerifericosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/perifericos/perifericos/PerifericosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.perifericos.perifericos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerifericosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
